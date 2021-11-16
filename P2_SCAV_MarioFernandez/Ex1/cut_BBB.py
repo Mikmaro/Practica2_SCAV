218b172f81f57e4a84ace04ec2d072682a592696
Mikmaro <83817639+Mikmaro@users.noreply.github.com>
2021-11-16 15:05:01 +0100

    P2

---
+++ P2_SCAV_MarioFernandez/Ex1/cut_BBB.py
@@ -0,0 +1,21 @@
+#Mario Fernández
+#Exercise 1
+
+"""
+En el terminal, debí hacer primero-->     pip install --trusted pypi.python.org moviepy
+
+Una vez hecho esto, encontré una líndea por internet que hacía exactamente lo
+que se nos pedía, así que compilando este script desde la terminal se creó el vídeo
+con el tiempo de corte deseado (para cambiar este corte se debe modificar la variable
+cut_video_frame)
+"""
+
+from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
+
+def cut_BBB(cut_time):
+    ffmpeg_extract_subclip("Big_Buck_Bunny.mp4", 0, cut_time, targetname="cut_video_12_seconds.mp4")
+
+
+if __name__ == '__main__':
+    cut_video_frame = 12
+    cut_BBB(cut_video_frame)
